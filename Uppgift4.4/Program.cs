759ddf4c7f36839b12293422965bf16632cb6189
arun06001 <arun06001@E-PW0234KE>
2023-10-18 12:59:40 +0200

    Add project files.

---
+++ Uppgift4.4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Uppgift4_4
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string svar;
+            int tal1;
+            int tal2;
+            bool cont = true;
+
+            do
+            {
+                Console.WriteLine("Välj ett av följande alternativ.\n1. Subtrahera ett tal med ett annat\n2. Dividera ett tal med ett annat\n3. Avsluta programmet");
+                svar = Console.ReadLine();
+
+                switch (svar)
+                {
+                    case "1":
+                        Console.WriteLine("Skriv in 2 tal på varsin linje som ska subtraheras");
+                        tal1 = int.Parse(Console.ReadLine());
+                        tal2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"Svaret är {tal1-tal2}.");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Skriv in 2 tal på varsin linje som ska divideras");
+                        tal1 = int.Parse(Console.ReadLine());
+                        tal2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"Svaret är {tal1 / tal2}.");
+                        break;
+
+                    case "3":
+                        cont = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Ogiltigt val!");
+                        break;
+                }
+            } while (cont);
+        }
+    }
+}
